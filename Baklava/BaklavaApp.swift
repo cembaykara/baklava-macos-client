aa5b53c49bc35350164c0c82fa18ce691058ff9c
Baris Cem Baykara <b.cembaykara@gmail.com>
2024-05-16 22:33:34 +0300

    Bump SDK version

--- Baklava/BaklavaApp.swift
+++ Baklava/BaklavaApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import BaklavaCore
 
 @main
 struct BaklavaApp: App {
+	
+	init() {
+		let config = BaklavaConfiguration(host: "127.0.0.1", port: 8080)
+		Baklava.setConfiguration(config)
+	}
+	
 	var body: some Scene {
 		MenuBarExtra("YAY", systemImage: "circle") {
 			MenuBarWindow()
